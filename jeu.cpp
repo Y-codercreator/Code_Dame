e8fadad093f8edd88c1a3ebc2114b7933f2d218c
LeMLeVrai <matisse.potier@stfelixlasalle.fr>
2025-03-11 17:27:20 +0100

    changement jeu

--- jeu.cpp
+++ jeu.cpp
@@ -2,5 +2,64 @@
 
 jeu::jeu()
 {
+    for(int y = 0; y < MAX; y++)
+    {
+        for(int x = 0; x < MAX; x++)
+        {
+            grille[y][x] = VIDE;
+        }
+    }
+}
 
+void jeu::init()
+{
+    for(int y = 0; y < 4; y++)
+    {
+        for(int x = 0; x < MAX; x+= 2)
+        {
+            if(y % 2 != 0 && x == 0)
+            {
+                x++;
+            }
+
+            grille[y][x] = NOIR;
+        }
+    }
+
+    for(int y = 6; y < MAX; y++)
+    {
+        for(int x = 0; x < MAX; x+= 2)
+        {
+            if(y % 2 != 0 && x == 0)
+            {
+                x++;
+            }
+
+            grille[y][x] = BLANC;
+        }
+    }
+}
+
+void jeu::detect_dame()
+{
+    if(j_mod == E_NOIR)
+    {
+        for(int x = 0 ; x < MAX; x += 2)
+        {
+            if(grille[9][x] == NOIR)
+            {
+                grille[9][x] = D_NOIRE;
+            }
+        }
+    }
+    else
+    {
+        for(int x = 1 ; x < MAX; x += 2)
+        {
+            if(grille[0][x] == BLANC)
+            {
+                grille[0][x] = D_BLANC;
+            }
+        }
+    }
 }
--- jeu.h
+++ jeu.h
@@ -1,11 +1,43 @@
 #ifndef JEU_H
 #define JEU_H
 
+#include <iostream>
+#include <string>
+
+#include "defines.h"
+
+typedef enum
+{
+    VIDE,       //Case vide
+    NOIR,       //Case pion noir
+    BLANC,      //Case pion blanc
+    D_NOIRE,    //Case dame noir
+    D_BLANC     //Case dame blanc
+}t_case;
+
+typedef enum
+{
+    E_BLANC,    //Joueur blanc
+    E_NOIR      //Joueur noir
+}t_joueur;
+
+typedef t_case t_grille[MAX][MAX];
 
 class jeu
 {
-public:
-    jeu();
+    private:
+        t_grille grille;
+        t_joueur j_mod;
+
+    public:
+        jeu();
+
+        //INITIALISATION DE LA GRILLE
+        void init();
+
+        //DETECTION ET MODIFICATIONS
+        void detect_dame();
+
 };
 
 #endif // JEU_H
